e65666506b95f833310ff963c47cf4013c9be0af
Vincent Villain <vincent.villain@instant-system.com>
2020-06-17 16:54:48 +0200

    add test for coverage

---
+++ bordeaux-webapp/src/test/java/com/instantsystem/bordeaux/BordeauxApplicationTest.java
@@ -0,0 +1,11 @@
+package com.instantsystem.bordeaux;
+
+import org.junit.Test;
+
+public class BordeauxApplicationTest {
+
+  @Test
+  public void testRunApp(){
+    new BordeauxApplication();
+  }
+}
